e76b4b0689380b200cac9e57c58142281df2f4a6
Jeffrey Booher-Kaeding <jeffbk95@gmail.com>
2019-04-07 15:41:26 -0700

    some minoir progress

--- segment/segment.go
+++ segment/segment.go
@@ -1,1 +1,26 @@
 package segment
+
+import (
+	"fmt"
+	"image"
+	_ "image/jpeg"
+
+	"gocv.io/x/gocv"
+)
+
+func segmentImage(in image.Image) (image.Gray, error) {
+	mat, _ := gocv.ImageToMatRGB(in)
+
+	/*
+		Do nerual net stuff here
+	*/
+
+	temp, _ := mat.ToImage()
+
+	mask, ok := temp.(*image.Gray)
+
+	if !ok {
+		return image.Gray{}, fmt.Errorf("not a Gray mat")
+	}
+	return *mask, nil
+}
--- segment/segment_test.go
+++ segment/segment_test.go
@@ -1,1 +1,42 @@
 package segment
+
+import (
+	"fmt"
+	"image"
+	_ "image/jpeg"
+	"log"
+	"os"
+	"testing"
+)
+
+func TestSegment(t *testing.T) {
+	println("beging test")
+	dest := "/Users/jeffdev/go/src/github.com/PhatFree/gokeh/images/digal_hiker_small.jpg"
+	testImg := loadImage(dest)
+
+	mask, err := segmentImage(testImg)
+
+	if err != nil {
+		fmt.Printf("There was an error: %s", err)
+	}
+
+	if mask.Opaque() {
+		t.Errorf("the mask is empty")
+	}
+
+	println("ending test")
+
+}
+
+func loadImage(file string) (img image.Image) {
+	reader, err := os.Open(file)
+	if err != nil {
+		println(file)
+		log.Fatalf("error loading %s: %s", file, err)
+	}
+	img, _, err = image.Decode(reader)
+	if err != nil {
+		log.Fatalf("error loading %s: %s", file, err)
+	}
+	return img
+}
