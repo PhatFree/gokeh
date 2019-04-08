07fd6cc37b4c8308a33ac705ed5cc24473090c6a
Jeffrey Booher-Kaeding <jeffbk95@gmail.com>
2019-04-07 22:39:20 -0700

    some PP done, still not testable.

--- segment/segment.go
+++ segment/segment.go
@@ -9,13 +9,33 @@ import (
 )
 
 func segmentImage(in image.Image) (image.Gray, error) {
-	mat, _ := gocv.ImageToMatRGB(in)
+
+	//load image into mat for openCV
+	//currently only wrriten to take RGB, grayscale would be easy to add
+	matIn, err := gocv.ImageToMatRGB(in)
+	if err != nil {
+		return image.Gray{}, err
+	}
+	matDes := gocv.NewMat()
+
+	net :=
+		gocv.ReadNetFromTensorflow("$GOPATH/src/github.com/PhatFree/gokeh/segment/tfModels/monodepth/model_city2eigen.pb")
+
+	//inputs needed
+	//
 
 	/*
 		Do nerual net stuff here
 	*/
 
-	temp, _ := mat.ToImage()
+	depSz := matDes.Size()
+
+	matDpth := gocv.NewMat()
+	matTemp := gocv.NewMatWithSizeFromScalar(gocv.NewScalar(0.3128, 0.3128, 0.3128, 0.3128), depSz[0], depSz[1], matDes.Type())
+	matDes.AddFloat(0.00001)
+	gocv.Divide(matTemp, matDes, &matDpth)
+
+	temp, _ := matDpth.ToImage()
 
 	mask, ok := temp.(*image.Gray)
 
